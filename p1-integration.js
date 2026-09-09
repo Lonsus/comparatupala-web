cb8a436a8e7ce61e5bcc2b4e2ca0d024379270db
Lonsus <130871515+Lonsus@users.noreply.github.com>
2026-09-09 03:16:14 +0200

    Finalize asset versions for price summary changes

--- p1-integration.js
+++ p1-integration.js
@@ -189,7 +189,7 @@
     if (!document.querySelector('link[data-progressive-disclosure]')) {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
-      link.href = 'progressive-disclosure.css?v=6';
+      link.href = 'progressive-disclosure.css?v=7';
       link.dataset.progressiveDisclosure = 'true';
       document.head.appendChild(link);
     }
