a99d7c42a3cf50ca8b22e54d4a33b2cf4bb07d9f
Lonsus <130871515+Lonsus@users.noreply.github.com>
2026-09-09 02:17:12 +0200

    chore: bump list layout stylesheet cache

--- p1-integration.js
+++ p1-integration.js
@@ -169,7 +169,7 @@
     if (!document.querySelector('link[data-progressive-disclosure]')) {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
-      link.href = 'progressive-disclosure.css?v=3';
+      link.href = 'progressive-disclosure.css?v=4';
       link.dataset.progressiveDisclosure = 'true';
       document.head.appendChild(link);
     }
